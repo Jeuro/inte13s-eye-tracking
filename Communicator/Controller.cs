fb7c94671e1b4827d4629493aaeba990f5ec8a0a
Eeva Terkki <juuuro@gmail.com>
2013-11-17 20:13:29 +0200

    Communicator fixed

---
+++ Communicator/Controller.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace eyeProject101 {
+
+    public class Controller {
+        private static EyetrackCommunicator comm = new EyetrackCommunicator();
+        public static void Main() {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo("en-US");
+            while (true) {
+                MirametrixDatum data = comm.GetData();
+                if (data != null) {
+                    Console.WriteLine(data.x + ", " + data.y);
+                }
+            }
+        }
+    }
+}
