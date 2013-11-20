11dd55d7a5ab05670a81da248ec4e9fcbce9ff4a
Simone <ssimxp@gmail.com>
2013-11-20 22:59:54 +0200

    Create MirametrixDatum.cs

---
+++ simone/MirametrixDatum.cs
@@ -0,0 +1,41 @@
+/*
+/* Author Eeva AkA Jeuro
+*/
+
+using System.Xml;
+using System.Globalization;
+using System.IO;
+using System;
+
+namespace eyeProject2
+{
+    public class MirametrixDatum 
+    {
+        public float? x { get; set; }
+        public float? y { get; set; }
+
+        public MirametrixDatum(string packet) 
+        {
+            
+            // Mirametrix data format: <REC FPOGX="0.000" FPOGY="0.000" FPOGS="39713.961" FPOGD="0.115" FPOGID="12443" FPOGV="0" />
+            if (packet == null) 
+            {
+                x = null;
+                y = null;
+            }
+            else 
+            {
+                using (XmlReader reader = XmlReader.Create(new StringReader(packet))) 
+                {
+                    try
+                    {
+                        reader.ReadToFollowing("REC");
+                        x = float.Parse(reader.GetAttribute("FPOGX"), CultureInfo.InvariantCulture);
+                        y = float.Parse(reader.GetAttribute("FPOGY"), CultureInfo.InvariantCulture);
+                    }
+                    catch (Exception) { }
+                }
+            }
+        }
+    }
+}
