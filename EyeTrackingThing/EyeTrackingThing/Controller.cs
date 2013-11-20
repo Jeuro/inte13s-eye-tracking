05a4a66a9a82829f21706272aee5c27ce070ba17
Eeva Terkki <juuuro@gmail.com>
2013-11-21 01:52:01 +0200

    project with new ahk class

---
+++ EyeTrackingThing/EyeTrackingThing/Controller.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EyeTrackingThing {
+
+    public class Controller {
+        private static EyetrackCommunicator comm = new EyetrackCommunicator();
+        public static void Main() {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo("en-US");
+            AutoHotkey ahk = new AutoHotkey();
+            ahk.Exec("Run notepad.exe");
+            while (true) {
+                MirametrixDatum data = comm.GetData();
+                if (data != null) {
+                    Console.WriteLine(data.x + ", " + data.y);
+                }
+            }
+        }
+    }
+}
