77c6cb6140de91ead3536397cdeffde2282bf68a
Sayantan Hore <sayantan.hore.86@gmail.com>
2013-11-27 17:28:57 +0200

    nowItWork

---
+++ wokr27112013/eyeProject2/eyeProject2/AutoHotkey.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eyeProject2{
+    class AutoHotkey {
+        [DllImport("AutoHotkey.dll", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Unicode, EntryPoint = "ahktextdll")]
+        private static extern IntPtr ahktextdll([MarshalAs(UnmanagedType.LPWStr)]string script,
+           [MarshalAs(UnmanagedType.LPWStr)]string options,
+           [MarshalAs(UnmanagedType.LPWStr)]string parameters);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool FreeLibrary(IntPtr hModule);
+
+        [DllImport("AutoHotkey.dll", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Unicode, EntryPoint = "ahkExec")]
+        private static extern bool ahkExec([MarshalAs(UnmanagedType.LPWStr)]string data);
+
+        private IntPtr handle;
+
+        public AutoHotkey() {
+            handle = ahktextdll("", "", "");
+        }
+
+        ~AutoHotkey() {
+            FreeLibrary(handle);
+        }
+
+        public bool Exec(string command) {
+            return ahkExec(command);
+        }
+    }
+}
---
+++ wokr27112013/eyeProject2/eyeProject2/ScriptHandler.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace eyeProject2
+{
+    class ScriptHandler
+    {
+        private Dictionary<string, List<string>> scripts;
+        private XDocument doc;
+        private AutoHotkey ahk;
+        public ScriptHandler(string file)
+        {
+            try
+            {
+                ahk = new AutoHotkey();
+                doc = XDocument.Load(file);
+                ReadScripts();
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine(e+"----------------------------------><----------------");
+            }
+        }
+
+        public void ReadScripts()
+        {
+            scripts = doc.Root.Elements().ToDictionary(x => (string) x.Attribute("menupos"),
+                                       x => new List<string>(
+                           x.Value.Split(new string[] { "\n" }, 
+                           StringSplitOptions.RemoveEmptyEntries)));
+        }
+
+        public void Execute(string item)
+        {
+            foreach (string command in scripts[item])
+            {
+                ahk.Exec(command);
+            }
+        }
+    }
+
+}
